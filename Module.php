f6801bb61c097bc4af7bc61380bb58c7cb6233e5
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-01-06 00:00:00 +0000

    Replaced dependency from module Generic to module Common.

--- Module.php
+++ Module.php
@@ -5,7 +5,7 @@
  * Add groups to users and resources to manage the access rights and the
  * resource visibility in a more flexible way.
  *
- * @copyright Daniel Berthereau, 2017-2023
+ * @copyright Daniel Berthereau, 2017-2025
  * @license http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
  *
  * This software is governed by the CeCILL license under French law and abiding
@@ -33,14 +33,13 @@
  */
 namespace Group;
 
-if (!class_exists(\Generic\AbstractModule::class)) {
-    require file_exists(dirname(__DIR__) . '/Generic/AbstractModule.php')
-        ? dirname(__DIR__) . '/Generic/AbstractModule.php'
-        : __DIR__ . '/src/Generic/AbstractModule.php';
+if (!class_exists(\Common\TraitModule::class)) {
+    require_once dirname(__DIR__) . '/Common/TraitModule.php';
 }
 
+use Common\Stdlib\PsrMessage;
+use Common\TraitModule;
 use Doctrine\ORM\Events;
-use Generic\AbstractModule;
 use Group\Controller\Admin\GroupController;
 use Group\Db\Event\Listener\DetachOrphanGroupEntities;
 use Group\Entity\Group;
@@ -66,6 +65,7 @@ use Omeka\Entity\ItemSet;
 use Omeka\Entity\Media;
 use Omeka\Entity\Resource;
 use Omeka\Entity\User;
+use Omeka\Module\AbstractModule;
 
 /**
  * Group
@@ -74,6 +74,8 @@ use Omeka\Entity\User;
  */
 class Module extends AbstractModule
 {
+    use TraitModule;
+
     const NAMESPACE = __NAMESPACE__;
 
     public function onBootstrap(MvcEvent $event): void
@@ -87,6 +89,20 @@ class Module extends AbstractModule
             Events::preFlush,
             new DetachOrphanGroupEntities
         );
+    }
+
+    protected function preInstall(): void
+    {
+        $services = $this->getServiceLocator();
+        $translate = $services->get('ControllerPluginManager')->get('translate');
+
+        if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.64')) {
+            $message = new \Omeka\Stdlib\Message(
+                $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
+                'Common', '3.4.65'
+            );
+            throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
+        }
     }
 
     /**
